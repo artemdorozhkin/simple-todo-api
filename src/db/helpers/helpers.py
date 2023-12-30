f09fa75e259ecb1dbf93564579e619d5b8a95d3d
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2023-12-30 14:47:03 +0300

    Removed unused module helpers.py

--- src/db/helpers/helpers.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def read_sql(path: Path):
-    if not path.suffix.lower() == '.sql':
-        raise Exception('file is not sql')
-    
-    with open(str(path.resolve()), 'r') as file:
-        return file.read()
