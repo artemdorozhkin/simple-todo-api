11d694a5d3a705dcf89b021eaec084832b45f9a6
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2023-12-30 14:48:05 +0300

    Moved db_utils and add query to create todos table

--- src/db/db_utils.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-from pathlib import Path
-
-
-DEFAULT_PATH = Path.joinpath(Path(__file__).parent, "default.db")
-def create_connection(db_path = DEFAULT_PATH):
-    return sqlite3.connect(db_path, check_same_thread=False)
