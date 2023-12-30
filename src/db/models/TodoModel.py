6af42bacfc27d0710a42b36af8d10b14f8eca132
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2023-12-30 14:14:07 +0300

    Changed TodoModel -> TodoService
    * added the _to_dict method
    * replaced the db to conn and cur

--- src/db/models/TodoModel.py
+++ src/db/models/TodoModel.py
@@ -1,24 +1,20 @@
 from datetime import datetime
-from pathlib import Path
-from string import Template
-from ..helpers.helpers import read_sql
-from db.DBService import DBService
+from sqlite3 import Connection, Row
 
 
-class TodoModel():
-    _db = None
-
-    def __init__(self, db: DBService) -> None:
-        self._db = db
+class TodoService():
+    def __init__(self, connection: Connection) -> None:
+        self.conn = connection
+        self.cur = self.conn.cursor()
 
     def create_todo(self, title: str, details: str, checked: bool):
         query = """
             INSERT INTO todos (title, details, checked)
             VALUES (?, ?, ?);
         """
-        data = self._db.cursor.execute(query, (title, details, checked))
-        self._db.connection.commit()
-        return data.fetchone()
+        id = self.conn.execute(query, (title, details, checked)).lastrowid
+        self.conn.commit()
+        return self.find_unique(id)
 
     def update_todo(self, id: int, title: str, details: str, checked: bool):
         query = """
@@ -29,31 +25,32 @@ class TodoModel():
                 updated_at = ?
             WHERE id = ?;
         """
-        data = self._db.cursor.execute(query, (
+        self.cur.execute(query, (
             title,
             details,
             checked,
             datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
             id
         ))
-        self._db.connection.commit()
-        return data.fetchone()
+        self.conn.commit()
+        return self.find_unique(id)
 
     def delete_todo(self, id: int):
         query = """
             DELETE FROM todos
             WHERE id = ?;
         """
-        data = self._db.cursor.execute(query, (id))
-        return data.fetchone()
+        data = self.find_unique(id)
+        self.cur.execute(query, (id,))
+        return data
 
     def find_all(self):
         query = """
             SELECT *
             FROM todos;
         """
-        data = self._db.cursor.execute(query)
-        return data.fetchall()
+        data = self.cur.execute(query)
+        return [self._to_dict(item) for item in data]
 
     def find_unique(self, id: int):
         query = """
@@ -61,6 +58,15 @@ class TodoModel():
             FROM todos
             WHERE id = ?;
         """
-        data = self._db.cursor.execute(query, (id))
-        return data.fetchone()
+        data = self.cur.execute(query, (id,))
+        return self._to_dict(data.fetchone())
 
+    def _to_dict(self, todo: tuple):
+        return {
+            "id": todo[0],
+            "title": todo[1],
+            "details": todo[2],
+            "checked": todo[3],
+            "created_at": todo[4],
+            "updated_at": todo[5],
+        }
