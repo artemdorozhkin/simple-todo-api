bf720b7ec92b0437e6341e1a8ff674fc0e5c3db5
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-01-02 12:30:31 +0300

    Refact TodoService
    * separated sql queries into files
    * if a single todo element is returned, it is returned without an array

--- api/services/todo.py
+++ api/services/todo.py
@@ -1,72 +1,55 @@
 from datetime import datetime
 from sqlite3 import Connection, Row
+import os
 
 
 class TodoService():
     def __init__(self, connection: Connection) -> None:
+        self.dirname = os.path.dirname(__file__)
         self.conn = connection
         self.cur = self.conn.cursor()
 
     def create_todo(self, title: str, details: str, checked: bool):
-        query = """
-            INSERT INTO todos (title, details, checked)
-            VALUES (?, ?, ?);
-        """
-        id = self.conn.execute(query, (title, details, checked)).lastrowid
+        data = {"title": title, "details": details, "checked": checked}
+        id = self.conn.execute(self._readsql("create_one.sql"), data).lastrowid
         self.conn.commit()
         return self.find_unique(id)
 
     def update_todo(self, id: int, title: str, details: str, checked: bool):
-        query = """
-            UPDATE todos
-            SET title = ?,
-                details = ?,
-                checked = ?,
-                updated_at = ?
-            WHERE id = ?;
-        """
-        self.cur.execute(query, (
-            title,
-            details,
-            checked,
-            datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
-            id
-        ))
+        data = {
+            "title": title,
+            "details": details,
+            "checked": checked,
+            "updated_at": datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
+            "id": id
+        }
+        self.cur.execute(self._readsql("update_one.sql"), data)
         self.conn.commit()
         return self.find_unique(id)
 
     def delete_todo(self, id: int):
-        query = """
-            DELETE FROM todos
-            WHERE id = ?;
-        """
-        data = self.find_unique(id)
-        self.cur.execute(query, (id,))
-        return data
+        item = self.find_unique(id)
+        data = {'id': id}
+        self.cur.execute(self._readsql("delete_one.sql"), data)
+        return item
 
     def find_all(self):
-        query = """
-            SELECT *
-            FROM todos;
-        """
-        data = self.cur.execute(query)
-        return [self._to_dict(item) for item in data]
+        data = self.cur.execute(self._readsql("select_all.sql"))
+        return self._to_dict(data.fetchall())
 
     def find_unique(self, id: int):
-        query = """
-            SELECT *
-            FROM todos
-            WHERE id = ?;
-        """
-        data = self.cur.execute(query, (id,))
+        data = {'id': id}
+        data = self.cur.execute(self._readsql("select_one.sql"), data)
         return self._to_dict(data.fetchone())
 
-    def _to_dict(self, todo: tuple):
-        return {
-            "id": todo[0],
-            "title": todo[1],
-            "details": todo[2],
-            "checked": todo[3],
-            "created_at": todo[4],
-            "updated_at": todo[5],
-        }
+    def _readsql(self, path: str):
+        fullpath = os.path.join(self.dirname, "sql", path)
+        with open(fullpath, "r") as sql:
+            return sql.read()
+
+    def _to_dict(self, todo):
+        columns = [col[0] for col in self.cur.description]
+        if isinstance(todo, tuple):
+            return dict(zip(columns, todo))
+        else:
+            return [dict(zip(columns, row)) for row in todo]
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from api.routes.todo import todo
 
-
 app = Flask(__name__)
 app.register_blueprint(todo)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host='localhost',
+        port=8080,
+        debug=True
+    )
