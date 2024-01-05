45e1ef56e77bf69f77d617b403f1f59a45d1f601
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-01-05 23:09:33 +0300

    Imported auth blueprint

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from dotenv import dotenv_values
 
 from api.routes.todo import todo
+from api.routes.auth import auth
 
 
 def create_app() -> Flask:
     config = dotenv_values('.env')
     app = Flask(__name__)
     app.register_blueprint(todo)
+    app.register_blueprint(auth)
     app.config['SECRET_KEY'] = config['SECRET_KEY']
 
     return app
