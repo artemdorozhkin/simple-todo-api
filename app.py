297436ceb403486ab9ff58e875a712bfe5d83fde
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-01-05 22:12:51 +0300

    Created create_app func in app.py && added dotenv

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from dotenv import dotenv_values
 
 from api.routes.todo import todo
 
 
-app = Flask(__name__)
+def create_app() -> Flask:
+    config = dotenv_values('.env')
+    app = Flask(__name__)
+    app.register_blueprint(todo)
+    app.config['SECRET_KEY'] = config['SECRET_KEY']
 
-app.register_blueprint(todo)
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(
         host='localhost',
         port=8080,
