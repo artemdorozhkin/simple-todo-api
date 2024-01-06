af5f8c6f4ea427109934f6148ef1619df21a0c79
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-01-06 21:05:37 +0300

    Splitted index func to index_get & index_post

--- api/routes/todo.py
+++ api/routes/todo.py
@@ -12,28 +12,31 @@ todo = Blueprint('todos', __name__, url_prefix="/todos")
 todo_serice = TodoService(db)
 
 
-@todo.route('/', methods=['GET', 'POST'])
+@todo.route('/', methods=['GET'])
 @jwt_required()
-def index():
+def index_get():
     current_app.logger.info("getting todos...")
-    if request.method == 'GET':
-        items = todo_serice.findall()
-        if len(items) == 0:
-            return make_response(items, NO_CONTENT)
-        else:
-            return make_response(items, OK)
+    items = todo_serice.findall()
+    if len(items) == 0:
+        return make_response(items, NO_CONTENT)
     else:
-        current_app.logger.info("creating todos...")
-        try:
-            todoitem = todo_serice.create(
-                title=request.form['title'],
-                details=request.form['details'],
-                checked=request.form['checked'],
-            )
-            return make_response(todoitem, CREATED)
-        except IncorrectData as e:
-            current_app.logger.error(e.args[0])
-            return http_response(BAD_REQUEST, {"msg": e.args[0]})
+        return make_response(items, OK)
+
+
+@todo.route('/', methods=['POST'])
+@jwt_required()
+def index_post():
+    current_app.logger.info("creating todos...")
+    try:
+        todoitem = todo_serice.create(
+            title=request.form['title'],
+            details=request.form['details'] or "",
+            checked=request.form['checked'] or False,
+        )
+        return make_response(todoitem, CREATED)
+    except IncorrectData as e:
+        current_app.logger.error(e.args[0])
+        return http_response(BAD_REQUEST, {"msg": e.args[0]})
 
 
 @todo.route('/<int:id>', methods=['GET'])
