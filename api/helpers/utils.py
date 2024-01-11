5ef2586a48a24d5d7f31151a1bdf665d3183eb2a
Artem Dorozhkin <aa.dorozhkin@ya.ru>
2024-01-11 23:31:17 +0300

    Updated utils.py
    Added funcs:
    * allowed_file: this func checks if the ext file exists in allowed extensions
    * generate_unique_filename: this func generates a unique filename using uuid
    * save_file: this func saves the file and returns generated name or an empty string

--- api/helpers/utils.py
+++ api/helpers/utils.py
@@ -1,3 +1,8 @@
+from os import path
+from typing import Callable
+from flask import Request
+
+
 def readsql(path: str):
     with open(path, "r") as sql:
         return sql.read()
@@ -9,3 +14,31 @@ def to_dict(description, result):
         return dict(zip(columns, result))
     else:
         return [dict(zip(columns, row)) for row in result]
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in [
+               'jpg', 'jpeg', 'png', 'bmp', 'gif'
+           ]
+
+
+def generate_unique_filename(filename: str):
+    _, ext = path.splitext(filename)
+
+    from uuid import uuid4
+    unique_filename = str(uuid4()) + ext
+    return unique_filename
+
+
+def save_file(files, folder):
+    filename = ''
+    if files:
+        file = files['image']
+        if allowed_file(file.filename):
+            filename = generate_unique_filename(file.filename)
+            file_path = path.join(
+                folder, filename
+            )
+            file.save(file_path)
+    return filename
